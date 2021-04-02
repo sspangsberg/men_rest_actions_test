592af10a2bef2c899f9e09d1ca7c96bb0ee0b485
sspangsberg <smsj@easv.dk>
2021-04-02 16:32:23 +0200

    Test 1

--- server.js
+++ server.js
@@ -9,6 +9,7 @@ const app = express();
 //load configuration from .env file
 require('dotenv-flow').config();
 
+
 //setup Swagger
 const swaggerDocument = YAML.load('./swagger.yaml');
 app.use('/api/docs', swaggerUi.serve, swaggerUi.setup(swaggerDocument));
